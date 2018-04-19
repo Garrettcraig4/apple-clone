80c1afcbccd262273dff69550e9b506577bde07e
Garrett <Garrettcrg@gmail.com>
2018-04-19 09:34:52 -0500

    added-ipad-to-home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,7 +3,7 @@ import "./Home.css";
 import red from "../../Asssetts/product_red_logo_alt_large.png";
 import hero from "../../Asssetts/hero_largetall.jpg";
 import iphonexhero from "../../Asssetts/iphone_x_largetall_2x.jpg";
-
+import ipadhero from "../../Asssetts/ipad_largetall.jpg";
 class Home extends Component {
   render() {
     return (
@@ -24,6 +24,11 @@ class Home extends Component {
           <p className="six"> Say hello to the future.</p>
 
           <img className="iximg" src={iphonexhero} />
+        </div>
+        <div className="ipad">
+          <p id="hipad">iPad</p>
+          <p id="sipad"> Like a computer. Unlike any computer. </p>
+          <img src={ipadhero} />
         </div>
       </div>
     );
