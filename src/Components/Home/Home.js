4a275f54e507f3811dbbe1ccb008b8bc0f3f4d27
Garrett <Garrettcrg@gmail.com>
2018-04-18 22:54:14 -0500

    added-red-iphone-and-iphone-x-to-home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,15 +2,29 @@ import React, { Component } from "react";
 import "./Home.css";
 import red from "../../Asssetts/product_red_logo_alt_large.png";
 import hero from "../../Asssetts/hero_largetall.jpg";
+import iphonexhero from "../../Asssetts/iphone_x_largetall_2x.jpg";
+
 class Home extends Component {
   render() {
     return (
-      <div className="Home">
-        <div className="se">
-          <p>Special Edition</p>
+      <div className="RealHome">
+        <div className="Home">
+          <div className="se">
+            <span>Special Edition</span>
+          </div>
+          <img id="red" src={red} />
+          <div id="two">
+            <p>Learn about (RED)® ></p>
+            <p>Buy ></p>
+          </div>
+          <img className="hero" src={hero} />
         </div>
-        <img src={red} />
-        <img className="hero" src={hero} />
+        <div className="iphonex">
+          <p className="ix">iPhone X</p>
+          <p className="six"> Say hello to the future.</p>
+
+          <img className="iximg" src={iphonexhero} />
+        </div>
       </div>
     );
   }
